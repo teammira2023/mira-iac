451e6fd29dafdb863138af065a9c35539eb38337
Bis Tripathy <54719690+bishuinit@users.noreply.github.com>
2023-03-24 13:46:25 -0400

    Update mytina.sh

--- mytina.sh
+++ mytina.sh
@@ -1,4 +1,4 @@
 echo $AWS_ACCESS_KEY_ID
 echo $AWS_SECRET_ACCESS_KEY
 echo $AWS_SESSION_TOKEN
-terraform apply -input=false -compact-warnings -auto-approve
+terraform destroy -input=false -compact-warnings -auto-approve
